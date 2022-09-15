661d669ad372870e0bf773dd19ce5e65be0956d2
Toothie <44488947+Toothie2213@users.noreply.github.com>
2022-09-15 20:22:59 -0300

    updated
    updated

--- SaveManager/Form1.cs
+++ SaveManager/Form1.cs
@@ -15,33 +15,38 @@ namespace SaveManager
 
         private void btnBrowse_Click(object sender, EventArgs e)
         {
-            using(var fbd = new FolderBrowserDialog()) { 
+            using (var fbd = new FolderBrowserDialog())
+            {
                 DialogResult result = fbd.ShowDialog();
 
                 if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
                 {
-                    if (Directory.Exists(fbd.SelectedPath + "\\ILSAVES"))
+                    if (Directory.Exists(fbd.SelectedPath))
                     {
-                        saves.Clear(); 
+                        path = fbd.SelectedPath;
+                        saves.Clear();
                         btnApply.Enabled = true;
                         comboBox1.Enabled = true;
 
-                        string[] files = Directory.GetFiles(fbd.SelectedPath + "\\ILSAVES");
-                        path = fbd.SelectedPath;
+                        string[] files = Directory.GetFiles(path);
 
-                        foreach(var f in files) { 
+
+                        foreach (var f in files)
+                        {
                             comboBox1.Items.Add(Path.GetFileName(f));
                             string key = Path.GetFileName(f);
                             saves.Add(key, f);
                         }
 
-                    } else {
-                        MessageBox.Show("ILSAVES folder not found", "Message");
+                    }
+                    else
+                    {
+                        MessageBox.Show("folder not found", "Message");
                         btnApply.Enabled = false;
-                        comboBox1.Enabled = false;   
+                        comboBox1.Enabled = false;
                         lblApplied.Text = "";
                         path = "";
-                    }               
+                    }
                 }
             }
         }
@@ -49,24 +54,44 @@ namespace SaveManager
         private void btnApply_Click(object sender, EventArgs e)
         {
             string req = comboBox1.Text;
-            string filePath = path + "\\SaveGame.save";
+            string defaultPath = "C:\\Users\\" + Environment.GetEnvironmentVariable("username") + "\\AppData\\LocalLow\\tinyBuild Games\\SaveGame.save";
+
             Process[] processes = Process.GetProcessesByName("Tinykin");
-            if (processes.Length != 0) { 
+            if (processes.Length != 0)
+            {
                 MessageBox.Show("Please close Tinykin before applying save");
                 return;
             }
 
-            if (saves.ContainsKey(req)) {
-                if(File.Exists(filePath)) { 
-                    File.Delete(filePath);
-                }
+            if (File.Exists(defaultPath))
+            {
+                if (saves.ContainsKey(req))
+                {
+                    if (File.Exists(defaultPath))
+                    {
+                        File.Delete(defaultPath);
+                    }
 
-                File.Copy(saves[req], filePath);
-                lblApplied.Text = "Applied successfully!";
-            } else { 
-                MessageBox.Show("Save was not found!", "Message");
-                lblApplied.Text = "";
-            }        
+                    File.Copy(saves[req], defaultPath);
+                    lblApplied.Text = "Applied successfully!";
+                }
+                else
+                {
+                    MessageBox.Show("Save was not found!", "Message");
+                    lblApplied.Text = "";
+                }
+            }
+            else
+            {
+                MessageBox.Show("Tinykin is not installed in its default location. If you see this contact Toothie#1111 on discord", "Message");
+            }
+
+
+        }
+
+        private void comboBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            lblApplied.Text = "";
         }
     }
 }
